10dd6e5fcdb3510556449a47a43d48ff83ea33dd
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2023-08-17 19:29:42 +0700

    Add project files.

---
+++ AmountException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTLap7._4Cshap
+{
+    public class AmountException:Exception
+    {
+        public AmountException():base("Invalid amount") { }
+        public AmountException(string message) : base(message) { }
+        
+        public AmountException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
